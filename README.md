7d770caa8db82d06da2bbef6385c9609017543a6
CodeCraftr <2283567+rstraub@users.noreply.github.com>
2021-07-08 14:40:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Kotlin starter project
+
+* Gradle 7
+* Kotlin 1.5.10
+* Kotest
+
+# Running Tests
+Run tests with:
+`gradle test`
+
+# Build
+Build with:
+`gradle build`
+
+Clean with:
+`gradle clean`
---
+++ src/test/kotlin/kata/base/AppTest.kt
@@ -0,0 +1,11 @@
+package kata.base
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class AppTest {
+    @Test
+    fun `should fail (junit)`() {
+        assertTrue(false);
+    }
+}
