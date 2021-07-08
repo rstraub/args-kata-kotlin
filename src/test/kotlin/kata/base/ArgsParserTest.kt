a58d114f6157b8d2e0a9cbceb30f169eb6aaed43
Roy Straub <roy.straub@codesquad.nl>
2021-07-08 15:37:29 +0200

    Before each

--- src/test/kotlin/kata/base/ArgsParserTest.kt
+++ src/test/kotlin/kata/base/ArgsParserTest.kt
@@ -2,26 +2,34 @@ package kata.base
 
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class ArgsParserTest {
+    private lateinit var parser: ArgsParser
+
+    @BeforeEach
+    internal fun setUp() {
+        parser = ArgsParser()
+    }
+
     @Test
     fun `should parse false given a unspecified boolean flag`() {
-        val result = ArgsParser() parse emptySet()
+        val result = parser parse emptySet()
 
         assertFalse(result)
     }
 
     @Test
     internal fun `should parse true given a specified boolean flag`() {
-        val result = ArgsParser() parse setOf("-l")
+        val result = parser parse setOf("-l")
 
         assertTrue(result)
     }
 
     @Test
     internal fun `should return false given an unknown specified boolean flag`() {
-        val result = ArgsParser() parse setOf("-x")
+        val result = parser parse setOf("-x")
 
         assertFalse(result)
     }
