ba4c94c522d086a647f0e138c56a359f97d2d4de
Roy Straub <roy.straub@codesquad.nl>
2021-08-18 10:18:02 +0200

    matches

---
+++ src/test/kotlin/kata/base/SchemaTest.kt
@@ -0,0 +1,20 @@
+package kata.base
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+internal class SchemaTest {
+    @Nested
+    inner class Matches {
+        @Test
+        fun `should return true if the arguments match the defined schema`() {
+            Schema(setOf("-l")) matches setOf("-l") shouldBe true
+        }
+
+        @Test
+        fun `should return false if the arguments do not match the defined schema`() {
+            Schema(setOf("-l")) matches setOf("-p") shouldBe false
+        }
+    }
+}
