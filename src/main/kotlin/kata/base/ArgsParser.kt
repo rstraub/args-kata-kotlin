469ae9ffc1615cbc6985d4f004218b0445f91ecb
Roy Straub <roy.straub@codesquad.nl>
2021-09-03 16:22:28 +0200

    little refactor

--- src/main/kotlin/kata/base/ArgsParser.kt
+++ src/main/kotlin/kata/base/ArgsParser.kt
@@ -1,16 +1,19 @@
 package kata.base
 
 class ArgsParser(private val schema: Schema) {
-
-    infix fun parse(arguments: Set<String>): Set<BooleanFlag> {
+    infix fun parse(arguments: Set<String>): Set<Flag<*>> {
         require(schema matches arguments)
         return schema
             .flags
-            .map { parseFlag(it.indicator, arguments) }
+            .map { parseFlag(it, arguments) }
             .toSet()
     }
 
-    private fun parseFlag(indicator: String, arguments: Set<String>) =
-        BooleanFlag(indicator, indicator in arguments)
+    private fun parseFlag(flag: Flag<*>, arguments: Set<String>) =
+        when (flag) {
+            is BooleanFlag -> BooleanFlag(flag.indicator, flag.indicator in arguments)
+            is NumericFlag -> flag
+            else -> throw IllegalArgumentException("Unsupported flag type")
+        }
 }
 
