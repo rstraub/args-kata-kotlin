78ca89a970a8b4489249a5c40473efdbcd94f146
Roy Straub <roy.straub@codesquad.nl>
2021-10-28 16:43:12 +0200

    Better split

--- src/test/kotlin/kata/base/ArgsParserTest.kt
+++ src/test/kotlin/kata/base/ArgsParserTest.kt
@@ -1,42 +1,56 @@
 package kata.base
 
-import io.kotest.matchers.collections.shouldBeIn
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class ArgsParserTest {
     private lateinit var parser: ArgsParser
 
-    @BeforeEach
-    internal fun setUp() {
-        parser = ArgsParser(Schema(BooleanFlag("-l"), BooleanFlag("-p")))
-    }
+    @Nested
+    inner class ArgumentsAndDefaults {
+        @BeforeEach
+        internal fun setUp() {
+            parser = ArgsParser(Schema(BooleanFlag("-l"), BooleanFlag("-p")))
+        }
 
-    @Test
-    fun `should return default values from schema given no arguments are supplied`() {
-        val result = parser parse ""
+        @Test
+        fun `should return default values from schema given no arguments are supplied`() {
+            val result = parser parse ""
 
-        BooleanFlag("-l", false) shouldBeIn result
-    }
+            result shouldBe setOf(
+                BooleanFlag("-l", false),
+                BooleanFlag("-p", false)
+            )
+        }
 
-    @Test
-    internal fun `should return values given some arguments are supplied`() {
-        val result = parser parse "-l"
+        @Test
+        internal fun `should return values given some arguments are supplied`() {
+            val result = parser parse "-l"
 
-        BooleanFlag("-l", true) shouldBeIn result
-    }
+            result shouldBe setOf(
+                BooleanFlag("-l", true),
+                BooleanFlag("-p", false)
+            )
+        }
 
-    @Test
-    internal fun `should return the values given all arguments are supplied`() {
-        val result = parser parse "-l -p"
+        @Test
+        internal fun `should return the values given all arguments are supplied`() {
+            val result = parser parse "-l -p"
 
-        BooleanFlag("-l", true) shouldBeIn result
-        BooleanFlag("-p", true) shouldBeIn result
+            result shouldBe setOf(
+                BooleanFlag("-l", true),
+                BooleanFlag("-p", true)
+            )
+        }
     }
 
     @Test
     internal fun `should throw exception given an unknown specified flag`() {
+        parser = ArgsParser(Schema())
+
         assertThrows<IllegalArgumentException> {
             parser parse "-x"
         }
